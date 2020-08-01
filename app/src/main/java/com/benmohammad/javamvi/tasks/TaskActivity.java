1b2f24b749f814be17c7b10d8efe057e69918849
Ben Mohammad <benmohammad593@gmail.com>
2020-08-01 13:59:06 +0100

    TaskActivity quick bug fix in fragment next commit...check...

--- app/src/main/java/com/benmohammad/javamvi/tasks/TaskActivity.java
+++ app/src/main/java/com/benmohammad/javamvi/tasks/TaskActivity.java
@@ -1,16 +1,77 @@
 package com.benmohammad.javamvi.tasks;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.benmohammad.javamvi.R;
+import com.benmohammad.javamvi.stats.StatisticsActivity;
+import com.benmohammad.javamvi.util.ActivityUtils;
+import com.google.android.material.navigation.NavigationView;
 
 public class TaskActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasks_act);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        assert ab != null;
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        if(navView != null) {
+            setUpDrawerContent(navView);
+        }
+        TasksFragment tasksFragment = (TasksFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if(tasksFragment == null) {
+            tasksFragment = TasksFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setUpDrawerContent(NavigationView navView) {
+        navView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    switch(menuItem.getItemId()) {
+                        case R.id.statistics_navigation_menu_item:
+                            Intent intent = new Intent(TaskActivity.this, StatisticsActivity.class);
+                            startActivity(intent);
+                            break;
+                        case R.id.list_navigation_menu_item:
+                            break;
+                        default:
+                            break;
+                    }
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawers();
+                    return true;
+                });
     }
 }
